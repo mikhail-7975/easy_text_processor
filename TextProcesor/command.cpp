13d90d97d37ed7bc001de93943b800c0c0fba110
Михаил Калиниченко <mikhail.kalinichenko.1999@gmail.com>
2019-12-16 14:52:42 +0700

    не изменяется doc->data() при изменении docData

--- TextProcesor/command.cpp
+++ TextProcesor/command.cpp
@@ -7,16 +7,16 @@ void Command::setDocument(std::shared_ptr<Document> _doc) {
 }
 
 void InsertCommand::Execute() {
-	std::string& docData = doc->data();
-	if (idx1 <= docData.size())
-		docData.insert(idx1, str);
+	//std::string& docData = doc->data();
+	if (idx1 <= docData->size())
+		docData->insert(idx1, str);
 	else
-		docData.insert(docData.size(), str);
+		docData->insert(docData->size(), str);
 }
 
 void InsertCommand::unExecute() {
-	std::string& docData = doc->data();
-	docData.erase(idx1, str.size());
+	//std::string& docData = doc->data();
+	docData->erase(idx1, str.size());
 }
 
 void isCorrectIndex(int& idx1, int& idx2, size_t size) {
@@ -31,21 +31,21 @@ void isCorrectIndex(int& idx1, int& idx2, size_t size) {
 }
 
 void DeleteCommand::Execute() {
-	std::string& docData = doc->data();
-	isCorrectIndex(idx1, idx2, docData.size());
-	delstr = docData.substr(idx1, idx2 - idx1 + 1);
-	docData.erase(idx1, idx2 - idx1 + 1);
+	//std::string& docData = doc->data();
+	isCorrectIndex(idx1, idx2, docData->size());
+	delstr = docData->substr(idx1, idx2 - idx1 + 1);
+	docData->erase(idx1, idx2 - idx1 + 1);
 }
 
 void DeleteCommand::unExecute() {
-	std::string& docData = doc->data();
-	docData.insert(idx1, delstr);
+	//std::string& docData = doc->data();
+	docData->insert(idx1, delstr);
 }
 
 void CopyCommand::Execute() {
 	std::string& docData = doc->data();
 	std::string& docBuffer = doc->buffer();
-	isCorrectIndex(idx1, idx2, docData.size()); 
+	isCorrectIndex(idx1, idx2, docData.size());
 	docBuffer = docData.substr(idx1, idx2 - idx1 + 1);
 }
 
@@ -53,7 +53,7 @@ void PasteCommand::Execute() {
 	std::string& docData = doc->data();
 	std::string& docBuffer = doc->buffer();
 	if (idx1 < 0) idx1 = 0;
-	if (idx1 > docData.size()) idx1 = docData.size();
+	if (idx1 > docData.size()) idx1 = docDatasize();
 	docData.insert(idx1, docBuffer);
 }
 
