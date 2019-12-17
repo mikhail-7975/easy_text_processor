9bce31c9ca0bbfe9a40ac7c1fade4f24ec372415
Михаил Калиниченко <mikhail.kalinichenko.1999@gmail.com>
2019-12-17 15:11:30 +0700

    заработало

--- TextProcesor/command.cpp
+++ TextProcesor/command.cpp
@@ -7,7 +7,6 @@ void Command::setDocument(std::shared_ptr<Document> _doc) {
 }
 
 void InsertCommand::Execute() {
-	//std::string& docData = doc->data();
 	if (idx1 <= docData->size())
 		docData->insert(idx1, str);
 	else
@@ -15,7 +14,6 @@ void InsertCommand::Execute() {
 }
 
 void InsertCommand::unExecute() {
-	//std::string& docData = doc->data();
 	docData->erase(idx1, str.size());
 }
 
@@ -31,35 +29,27 @@ void isCorrectIndex(int& idx1, int& idx2, size_t size) {
 }
 
 void DeleteCommand::Execute() {
-	//std::string& docData = doc->data();
 	isCorrectIndex(idx1, idx2, docData->size());
 	delstr = docData->substr(idx1, idx2 - idx1 + 1);
 	docData->erase(idx1, idx2 - idx1 + 1);
 }
 
 void DeleteCommand::unExecute() {
-	//std::string& docData = doc->data();
 	docData->insert(idx1, delstr);
 }
 
 void CopyCommand::Execute() {
-	//std::string& docData = doc->data();
-	//std::string& docBuffer = doc->buffer();
+	std::string& _docBuffer = doc->buffer();
 	isCorrectIndex(idx1, idx2, docData->size());
-	docBuffer.reset(&docData->substr(idx1, idx2 - idx1 + 1));
-	//docData->substr(idx1, idx2 - idx1 + 1)
+	_docBuffer = docData->substr(idx1, idx2 - idx1 + 1);
 }
 
 void PasteCommand::Execute() {
-	//std::string& docData = doc->data();
-	//std::string& docBuffer = doc->buffer();
 	if (idx1 < 0) idx1 = 0;
 	if (idx1 > docData->size()) idx1 = docData->size();
 	docData->insert(idx1, *docBuffer);
 }
 
 void PasteCommand::unExecute() {
-	//std::string& docData = doc->data();
-	//std::string& docBuffer = doc->buffer();
 	docData->erase(idx1, idx1 + docBuffer->size());
 }
